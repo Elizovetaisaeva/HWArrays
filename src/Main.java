6d7814690fdf8b1d96073ccbf4d9b8ca08522342
Лаисат <lz_256@mail.ru>
2023-11-06 12:57:18 +0300

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -2,74 +2,70 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
-        task4();
+        taskPart1();
+
 
     }
 
-    public static void task1() {
+    public static void taskPart1() {
+        // "Задание 1"
+
         int[] firstArray = new int[]{1, 2, 3};
         double[] secondArray = new double[]{1.57, 7.654, 9.986};
         char[] thirdArray = new char[]{'a', 'b', 'c'};
 
+        System.out.println("Задание 2");
 
-    }
-
-
-    public static void task2() {
-        int[] firstArray = new int[0];
         for (int i = 0; i < firstArray.length; i++) {
             System.out.println(firstArray[i]);
 
             if (i != firstArray.length - 1) {
-                System.out.println(",");
+                System.out.print(",");
             }
-        }
+
+    }
         System.out.println();
-        int[] secondArray = new int[0];
+
+
         for (int i = 0; i < secondArray.length; i++) {
             System.out.println(secondArray[i]);
 
             if (i != secondArray.length - 1) {
-                System.out.println(",");
+                System.out.print(",");
             }
         }
 
         System.out.println();
-        int[] thirdArray = new int[0];
+
         for (int i = 0; i < thirdArray.length; i++) {
             System.out.println(thirdArray[i]);
 
             if (i != thirdArray.length - 1) {
-                System.out.println(",");
+                System.out.print(",");
             }
         }
-    }
 
+        System.out.println("Задание 3");
 
-        public static void task3() {
-            int[] firstArray = new int[0];
             for (int i = firstArray. length -1; i>=0; i--) {
                 System.out.println(firstArray[i]);
 
                 if (i != 0) {
-                    System.out.println(",");
+                    System.out.print(",");
                 }
             }
-            System.out.println();
-            int[] secondArray = new int[0];
+        System.out.println();
+
             for (int i = secondArray. length -1; i>=0; i--) {
                 System.out.println( secondArray [i]);
 
                 if (i != 0) {
-                    System.out.println(",");
+                    System.out.print(",");
                 }
             }
 
-            System.out.println();
-            int[] thirdArray = new int[0];
+        System.out.println();
+
             for (int i = thirdArray. length -1; i>=0; i--) {
                 System.out.println(thirdArray[i]);
 
@@ -77,16 +73,12 @@ public class Main {
                     System.out.println(",");
                 }
             }
-            System.out.println();
-    }
+            System.out.println("Задание 4");
 
-
-
-    public static void task4() {
-        int[] arr = new int[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 != 0) {
-                arr [i] ++;
+        for (int i = 0; i < thirdArray.length; i++) {
+            if (thirdArray[i] % 2 != 0) {
+                thirdArray [i] ++;
+                System.out.print(Arrays.toString(thirdArray));
             }
         }
         }
